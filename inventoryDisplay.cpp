427762d4254cda61634dd6895f64311e31d89334
Judd Morrison <juddm8@msn.com>
2013-04-23 19:39:55 +1000

    Cleanup

--- inventoryDisplay.cpp
+++ inventoryDisplay.cpp
@@ -1,9 +1,7 @@
 #include "functions.h"
 
 void inventoryDisplay(characterInfo player) {
-    
     //
-    
     cout<< "================================================================================";
     cout<< "|Health :                                                                      |";
     cout<< "|Mana   :                                                                      |";
@@ -28,29 +26,21 @@ void inventoryDisplay(characterInfo player) {
     cout<< "|                                                                              |";
     cout<< "|                                                                              |";
     cout<< "================================================================================";
-    
     cursorPos(11, 4);
     cout<<player.gold;
-    
     inventoryDisplayHealth(player);
     inventoryDisplayMana(player);
     inventoryDisplaySlots(player);
     inventoryDisplayArmor(player);
-    
     if(player.hasBackpack = true) {
         inventoryDisplayBackpack(player);
     }
-    
     cursorPos(2, 18);
-    
     //
-    
 }
 
 void inventoryDisplaySlots(characterInfo player) {
-    
     //
-    
     cursorPos(10, 6);
     cout<<player.playerInv.slot1.name;
     cursorPos(10, 7);
@@ -61,15 +51,11 @@ void inventoryDisplaySlots(characterInfo player) {
     cout<<player.playerInv.slot4.name;
     cursorPos(10, 10);
     cout<<player.playerInv.slot5.name;
-    
     //
-    
 }
 
 void inventoryDisplayArmor(characterInfo player) {
-    
     //
-    
     cursorPos(14, 12);
     cout<<player.playerArmor.slot1.name;
     cursorPos(14, 13);
@@ -80,15 +66,11 @@ void inventoryDisplayArmor(characterInfo player) {
     cout<<player.playerArmor.slot4.name;
     cursorPos(14, 16);
     cout<<player.playerArmor.slot5.name;
-    
     //
-    
 }
 
 void inventoryDisplayBackpack(characterInfo player) {
-    
     //
-    
     cursorPos(10, 6);
     cout<<player.playerBackpack.slot1.name;
     cursorPos(10, 7);
@@ -99,115 +81,67 @@ void inventoryDisplayBackpack(characterInfo player) {
     cout<<player.playerBackpack.slot4.name;
     cursorPos(10, 10);
     cout<<player.playerBackpack.slot5.name;
-    
     //
-    
 }
 
 void inventoryDisplayHealth(characterInfo player) {
-    
     //
-    
     cursorPos(11, 2);
-    
     setColour(1, 16);
-    
     if(player.health >= 100) {
-        
         cout<<player.health << "/" <<player.totalHealth;
-        
     } else if(player.health > 0) {
-        
         cout<<player.health << "/" <<player.totalHealth<< " ";
-        
     } else {
-        
         cout<<player.health << "/" <<player.totalHealth<< "  ";
-        
     }
     
     setColour(15, 1);
     cout<< ":";
-    
     setColour(5, 16);
     int healthBar1 = player.totalHealth / 10;
-    
     for(int i = 0; i < healthBar1; i++) {
-        
         cout<< " ";
-        
     }
-    
     setColour(15, 1);
     cout<< ":";
-    
     cursorPos(19, 2);
-    
     setColour(13, 16);
     int healthBar2 = player.health / 10;
-    
     for(int i = 0; i < healthBar2; i++) {
-        
         cout<< " ";
-        
     }
-    
     setColour(1, 8);
-    
     //
-    
 }
 
 void inventoryDisplayMana(characterInfo player) {
-    
     //
-    
     cursorPos(11, 3);
-    
     setColour(1, 16);
-    
     if(player.mana >= 100) {
-        
         cout<<player.mana << "/" <<player.totalMana;
-        
     } else if(player.mana > 0) {
-        
         cout<<player.mana << "/" <<player.totalMana<< " ";
-        
     } else {
-        
         cout<<player.mana << "/" <<player.totalMana<< "  ";
-        
     }
     
     setColour(15, 1);
     cout<< ":";
-        
     setColour(2, 16);
     int manaBar1 = player.totalMana / 10;
-    
     for(int i = 0; i < manaBar1; i++) {
-        
         cout<< " ";
-        
     }
-    
     setColour(15, 1);
     cout<< ":";
-    
     cursorPos(19, 3);
-    
     setColour(10, 16);
     int manaBar2 = player.mana / 10;
-    
     for(int i = 0; i < manaBar2; i++) {
-        
         cout<< " ";
-        
     }
-    
     setColour(1, 8);
-    
     //
-    
 }
