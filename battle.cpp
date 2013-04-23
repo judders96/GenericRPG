16ecd56c5c3b4a09c6d8fd70b4895420c27c2487
Judd Morrison <juddm8@msn.com>
2013-04-23 17:32:22 +1000

    final fix

---
+++ battle.cpp
@@ -0,0 +1,70 @@
+#include "functions.h"
+
+
+characterInfo battle(characterInfo player) {
+    
+    //
+    
+    enemyInfo enemy = enemyInit();
+    
+    //
+    
+    system("cls");
+    cout<< "================================================================================";
+    cout<< "|Player name   :                                                               |";
+    cout<< "|Player health :                                                               |";
+    cout<< "|Player mana   :                                                               |";
+    cout<< "|==============================================================================|";
+    cout<< "|Enemy name    :                                                               |";
+    cout<< "|Enemy health  :                                                               |";
+    cout<< "|Enemy mana    :                                                               |";
+    cout<< "|==============================================================================|";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "|                                                                              |";
+    cout<< "================================================================================";
+    battleDisplay(player, enemy);
+    
+    while(player.health > 0 && enemy.health > 0) {
+    
+        cout<< "|Battle!" <<endl<<"|";
+        
+        player.health = deductHealth(player.health, enemy.baseDamage);
+        enemy.health = deductHealth(enemy.health, player.baseDamage);
+        
+        system("pause");
+        
+        battleDisplay(player, enemy);
+        
+    }
+    
+    cout<< "|Battle over" <<endl<< "|";
+    
+    if(player.health > 0 && enemy.health == 0) {
+        
+        cout<< "You win!" <<endl<< "|";
+        
+    } else {
+        
+        cout<< "You lose!" <<endl<< "|";
+        
+    }
+    
+    return player;
+    
+    system("cls");
+    
+    //
+    
+}
---
+++ characterInit.cpp
@@ -0,0 +1,124 @@
+#include "functions.h"
+
+characterInfo characterInit(int newLoad) {
+    
+    //
+    
+    ifstream inFile;
+    
+    characterInfo playerInit;
+    
+    playerInit.maxHealth = 500;
+    playerInit.maxMana = 500;
+    playerInit.hasBackpack = false;
+    
+    
+    switch(newLoad) {
+        case 1:
+            playerInit = newGame(playerInit);
+            break;
+        case 2:
+            int fileSelect;
+            
+            inFile.open("Save1.txt");
+            cout<< "Save 1: ";
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.name;
+            cout<<playerInit.name<<endl;
+            inFile.close();
+            
+            inFile.open("Save2.txt");
+            cout<< "Save 2: ";
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.name;
+            cout<<playerInit.name<<endl;
+            inFile.close();
+            
+            inFile.open("Save3.txt");
+            cout<< "Save 3: ";
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.name;
+            cout<<playerInit.name<<endl;
+            inFile.close();
+            
+            cout<< "Which save file would you like to choose? " <<endl;
+            cin>>fileSelect;
+            
+            cout<< "Loading file..." <<endl;
+            Sleep(750);
+            
+            switch(fileSelect) {
+                case 1:
+                    inFile.open("Save1.txt");
+                    break;
+                case 2:
+                    inFile.open("Save2.txt");
+                    break;
+                case 3:
+                    inFile.open("Save3.txt");
+                    break;
+            }
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.name;
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.revivesLeft;
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.totalHealth;
+            playerInit.health = playerInit.totalHealth;
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.totalMana;
+            playerInit.mana = playerInit.totalMana;
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.baseDamage;
+            inFile.ignore(256, ' ');
+            inFile>>playerInit.gold;
+            
+            inFile.close();
+            break;
+    }
+    
+    return playerInit;
+    
+    system("cls");
+    
+    //
+    
+}
+
+characterInfo newGame(characterInfo playerInit) {
+    
+    //
+    
+    string menu;
+    
+    playerInit.revivesLeft = 3;
+    playerInit.totalHealth = 100;
+    playerInit.health = playerInit.totalHealth;
+    playerInit.totalMana = 100;
+    playerInit.mana = playerInit.totalMana;
+    playerInit.baseDamage = 30;
+    playerInit.gold = 50;
+    
+    do {
+        slowPrint("Please enter your name, Traveller");
+        cin>>playerInit.name;
+        cout<<playerInit.name<< ", this is your name(y/n)?";
+        cin>>menu;
+        system("cls");
+    } while(menu != "y");
+    
+    slowPrint("Welcome, to the world of Genericia, " + playerInit.name);
+    cout<<endl;
+    cout<< "(1)Human" <<endl;
+    cout<< "(2)Orc" <<endl;
+    cout<< "(3)Arcanian" <<endl;
+    cout<< "(4)Reptilian" <<endl;
+    cout<< "(5)Constructiod" <<endl;
+    slowPrint("Please choose a race: ");
+    cin>>playerInit.playerRace.raceId;
+    
+    return playerInit;
+    
+    //
+    
+}
